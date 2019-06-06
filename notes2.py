ddb2c6b745eb464c856759577818abb99491058a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-06 12:45:58 -0400

    Goes through the Polya method for writing an algorithm for Fibonacci sequence

--- notes2.py
+++ notes2.py
@@ -1,5 +1,11 @@
 #  Fibonacci Sequence
 
+#  Write a function that computes the nth Fibonacci number.
+
+#----------Clarifying the Problem------------
+
+#  n is a non-negative integer
+
 #----------Understand the Problem------------
 
 #  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...]
@@ -8,3 +14,18 @@
 #  F(n) = F(n-1) + F(n-2)
 
 #  F(8) = 21
+
+#--------------Devise a Plan-----------------
+
+def fib(n):
+    # TODO make sure n is non-negative 
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return fib(n-1) + fib(n-2)
+  
+for i in range(40):
+    print(f'{i}: {fib(i)}')
+    
